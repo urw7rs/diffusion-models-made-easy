abe353eaddfe928341fc1c801d7cbfac95709894
urw7rs <urw.7rs@icloud.com>
2022-12-29 19:09:36 +0900

    add classifier guidance for DDPM and DDIM

--- src/dmme/__init__.py
+++ src/dmme/__init__.py
@@ -1,10 +1,11 @@
-__all__ = ["ddpm", "ddim", "adm", "data", "trainer"]
+__all__ = ["ddpm", "ddim", "adm", "guidance", "data", "trainer"]
 __version__ = "0.3.0"
 
 from . import ddpm
 from . import ddim
 
 from . import adm
+from . import guidance
 
 from . import data
 
