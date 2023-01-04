693931fe0be3e529900cdde7556cf1dd7d029925
urw7rs <urw.7rs@icloud.com>
2023-01-05 00:20:55 +0900

    fix cosine schedule: cos was used instead of cos^2

--- src/dmme/equations/iddpm/iddpm.py
+++ src/dmme/equations/iddpm/iddpm.py
@@ -5,7 +5,7 @@ import torch
 
 def cosine_schedule(timesteps, offset):
     def f(t):
-        return torch.cos((t / timesteps + offset) / (1 + offset) * math.pi / 2)
+        return torch.cos((t / timesteps + offset) / (1 + offset) * math.pi / 2) ** 2
 
     t = torch.arange(0, timesteps + 1)
     zero = torch.tensor([0], dtype=torch.float32)
