b149e7aa7f825412a4d0af1bcbb32339a49edc81
urw7rs <urw.7rs@icloud.com>
2023-01-07 23:13:41 +0900

    filter missing data

--- src/dmme/datasets/lsun.py
+++ src/dmme/datasets/lsun.py
@@ -1,8 +1,80 @@
 from typing import Any, cast, Callable, List, Optional, Tuple, Union
 
 import os.path as osp
+import io
+import pickle
+import string
 
-from torchvision.datasets import VisionDataset, LSUNClass
+import PIL
+from PIL import Image
+
+from torchvision.datasets import VisionDataset
+
+
+class LSUNClass(VisionDataset):
+    def __init__(
+        self,
+        root: str,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+    ) -> None:
+        import lmdb
+
+        super().__init__(root, transform=transform, target_transform=target_transform)
+
+        self.env = lmdb.open(
+            root,
+            max_readers=1,
+            readonly=True,
+            lock=False,
+            readahead=False,
+            meminit=False,
+        )
+        with self.env.begin(write=False) as txn:
+            self.length = txn.stat()["entries"]
+        cache_file = "_cache_" + "".join(c for c in root if c in string.ascii_letters)
+        if osp.isfile(cache_file):
+            self.keys = pickle.load(open(cache_file, "rb"))
+        else:
+            print("creating cache, this may take a while...")
+
+            with self.env.begin(write=False) as txn:
+                self.keys = []
+                for key, value in txn.cursor():
+                    self.keys.append(key)
+
+                    try:
+                        buf = io.BytesIO()
+                        buf.write(value)
+                        Image.open(buf)
+                    except PIL.UnidentifiedImageError:
+                        # skip invalid values
+                        print(f"skipped {key}")
+                        self.keys.pop()
+
+            pickle.dump(self.keys, open(cache_file, "wb"))
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        img, target = None, None
+        env = self.env
+        with env.begin(write=False) as txn:
+            imgbuf = txn.get(self.keys[index])
+
+        buf = io.BytesIO()
+        buf.write(imgbuf)
+        buf.seek(0)
+        img = Image.open(buf).convert("RGB")
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self) -> int:
+        return len(self.keys)
 
 
 class LSUN(VisionDataset):
