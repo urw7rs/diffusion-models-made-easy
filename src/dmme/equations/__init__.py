209505824d4a47f050b1329cc1ecc89b4c0e8b7e
urw7rs <urw.7rs@icloud.com>
2023-01-08 18:37:20 +0900

    add classifier grad

--- src/dmme/equations/__init__.py
+++ src/dmme/equations/__init__.py
@@ -1,5 +1,6 @@
 from dmme.equations import ddpm
 from dmme.equations import ddim
 from dmme.equations import iddpm
+from dmme.equations import classifier_guidance
 
-__all__ = ["ddpm", "ddim", "iddpm"]
+__all__ = ["ddpm", "ddim", "iddpm", "classifier_guidance"]
