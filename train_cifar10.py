730cc1710ac6a72a1b7056a7813e3dcede3271eb
urw7rs <urw.7rs@icloud.com>
2023-01-16 01:01:10 +0900

    add cifar10 dataset using pytorch dataloading and ema, grad clip, warmup schedule

--- train_cifar10.py
+++ train_cifar10.py
@@ -1,20 +1,78 @@
 import time
 
+import numpy as np
+
 from jax import jit
 from jax import random
 import jax.numpy as jnp
 
 import optax
 
+from torch.utils import data
+
+from torchvision import datasets
+import torchvision.transforms as TF
+
 from dmme import ddpm
 
 batch_size = 128
 height = width = 32
 channels = 3
+timesteps = 1000
 
 learning_rate = 2e-4
-timesteps = 1000
+grad_clip_norm = 1.0
+ema_decay = 0.999
+warmup_steps = 5000
 train_iterations = 800_000
+
+
+def numpy_collate(batch):
+    if isinstance(batch[0], np.ndarray):
+        return np.stack(batch)
+    elif isinstance(batch[0], (tuple, list)):
+        transposed = zip(*batch)
+        return [numpy_collate(samples) for samples in transposed]
+    else:
+        return np.array(batch)
+
+
+class NumpyLoader(data.DataLoader):
+    def __init__(
+        self,
+        dataset,
+        batch_size=1,
+        shuffle=False,
+        sampler=None,
+        batch_sampler=None,
+        num_workers=0,
+        pin_memory=False,
+        drop_last=False,
+        timeout=0,
+        worker_init_fn=None,
+    ):
+        super(self.__class__, self).__init__(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            sampler=sampler,
+            batch_sampler=batch_sampler,
+            num_workers=num_workers,
+            collate_fn=numpy_collate,
+            pin_memory=pin_memory,
+            drop_last=drop_last,
+            timeout=timeout,
+            worker_init_fn=worker_init_fn,
+        )
+
+
+class Cast(object):
+    def __call__(self, pic):
+        return np.array(pic, dtype=jnp.float16)
+
+
+def norm(x):
+    return (x - 0.5) * 2
 
 
 def main(seed):
@@ -38,31 +96,61 @@ def main(seed):
     state, params = variables.pop("params")
     del variables
 
+    learning_rate_schedule = optax.join_schedules(
+        [
+            optax.linear_schedule(
+                init_value=learning_rate / warmup_steps,
+                end_value=learning_rate,
+                transition_steps=warmup_steps,
+            ),
+        ],
+        [warmup_steps],
+    )
     state = ddpm.TrainState.create(
         apply_fn=model.apply,
         params=params,
         dropout_key=dropout_key,
         timestep_key=timestep_key,
         noise_key=noise_key,
-        tx=optax.adam(learning_rate),
+        tx=optax.chain(
+            optax.clip_by_global_norm(grad_clip_norm),
+            optax.adam(learning_rate=learning_rate_schedule),
+            optax.ema(decay=ema_decay),
+        ),
     )
 
     schedule = ddpm.LinearSchedule.create(timesteps)
 
-    x = random.normal(key, shape=(batch_size, height, width, channels))
-
     train_step_jitted = jit(ddpm.train_step)
 
-    t0 = time.perf_counter()
-    for i in range(train_iterations):
-        loss, state = train_step_jitted(
-            state, schedule.alpha_bar, schedule.timesteps, x
-        )
+    train_set = datasets.CIFAR10(
+        root=".",
+        train=True,
+        transform=TF.Compose([TF.RandomHorizontalFlip(), Cast(), norm]),
+        download=True,
+    )
 
-        if i % 100 == 0:
-            t = time.perf_counter() - t0
-            print(f"loss: {loss} {100 / t} it/s")
-            t0 = time.perf_counter()
+    dataloader = NumpyLoader(
+        train_set, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=True
+    )
+
+    t0 = time.perf_counter()
+    step = 1
+    while step < train_iterations:
+        for x, y in dataloader:
+            if step > train_iterations:
+                break
+
+            loss, state = train_step_jitted(
+                state, schedule.alpha_bar, schedule.timesteps, x
+            )
+
+            if step % 100 == 0:
+                t = time.perf_counter() - t0
+                print(f"loss: {loss} {100 / t} it/s")
+                t0 = time.perf_counter()
+
+            step += 1
 
 
 if __name__ == "__main__":
